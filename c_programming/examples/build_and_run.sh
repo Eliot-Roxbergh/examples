66cc3c6c52d8e9784304cfde83808c1b4e9535f4
Eliot Roxbergh <eliot.roxbergh@ericsson.com>
2022-01-13 12:23:54 +0100

    C code: reordering and added development_tips

---
+++ c_programming/examples/build_and_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+#Quick script to build and run a C project with some useful flags
+# Note -g shouldn't be used in production etc. etc. :)
+
+target="$@" #allow multiple source files (from arguments) #shellcheck complains on this line: https://github.com/koalaman/shellcheck/wiki/SC2124
+
+debug="-g -fasynchronous-unwind-tables -fexceptions -Wall -Werror -pedantic -Wconversion -Wextra -Wformat=2 -Wformat-truncation -Wunused -Werror=implicit-function-declaration -Werror=format-security"
+#-Wunused-result seem to only warn for special functions return?
+
+echo "Running:"
+echo "gcc -std=c11 -Wall -Werror -pedantic -Wconversion ${debug} ${target} -o a.out && ./a.out"
+echo
+gcc -std=c11 -Wall -Werror -pedantic -Wconversion ${debug} ${target} -o a.out && ./a.out
+
